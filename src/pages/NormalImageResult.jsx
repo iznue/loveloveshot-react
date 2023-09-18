a53d0d4926d301bf5a98f7089ae74072da576bae
wanny <kko6369@naver.com>
2023-09-18 14:44:06 +0900

    Feat : normal 편집 결과

--- src/pages/NormalImageResult.jsx
+++ src/pages/NormalImageResult.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import "../assets/pages/normalImageResult.css";
 
 const NormalImageResult = () => {
   const location = useLocation();
   const [resultImage, setResultImage] = useState();
   const navigate = useNavigate();
-  const serverURL = "http://192.168.0.159:8080";
+  const serverURL = "http://192.168.0.21:8080";
 
   useEffect(() => {
     if (!location || !location.state || !location.state.result) {
